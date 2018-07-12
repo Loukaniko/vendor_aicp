6e043839e121dd46c180517682f988e8e429b1e2
dkati <dkatikaridis@gmail.com>
2018-07-12 10:31:08 +0300

    Do not grab officially supported devices
    Change-Id: I7403f95671cba492ac78deefa0ab5d327a701661

--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
-for combo in $(curl -s https://raw.githubusercontent.com/AICP/vendor_jenkins/o8.1/aicp-build-targets   | sed -e 's/#.*$//' | grep o8.1 | awk {'print $2'})
-do
-    add_lunch_combo $combo
-done
